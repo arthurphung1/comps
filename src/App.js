5ca60c5289d9c95c58ac70dd24e4a28d2b96d5a5
arthurphung1 <rthrphung73@gmail.com>
2023-06-05 15:57:54 -0400

    finished button comp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
+import Button from './Button';
+
 function App() {
-    return <div>App</div>;
+    return (
+        <div>
+            <div>
+                <Button
+                    success
+                    rounded
+                    outline
+                >
+                    <GoBell />
+                    Click me!!
+                </Button>
+            </div>
+            <div>
+                <Button
+                    danger
+                    outline
+                >
+                    <GoCloudDownload />
+                    Buy Now!
+                </Button>
+            </div>
+            <div>
+                <Button warning>
+                    <GoDatabase />
+                    See Deal!
+                </Button>
+            </div>
+            <div>
+                <Button
+                    secondary
+                    outline
+                >
+                    Hide Ads!
+                </Button>
+            </div>
+            <div>
+                <Button
+                    primary
+                    rounded
+                >
+                    Something!
+                </Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
