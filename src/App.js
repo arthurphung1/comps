a8a5e9c285422ba93c826cf4caf0aa54e8bef6cc
arthurphung1 <rthrphung73@gmail.com>
2023-06-05 22:29:16 -0400

    added accordion comp

--- src/App.js
+++ src/App.js
@@ -1,52 +1,25 @@
-import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
-import Button from './Button';
+import Accordion from './components/Accordion';
 
 function App() {
-    return (
-        <div>
-            <div>
-                <Button
-                    success
-                    rounded
-                    outline
-                >
-                    <GoBell />
-                    Click me!!
-                </Button>
-            </div>
-            <div>
-                <Button
-                    danger
-                    outline
-                >
-                    <GoCloudDownload />
-                    Buy Now!
-                </Button>
-            </div>
-            <div>
-                <Button warning>
-                    <GoDatabase />
-                    See Deal!
-                </Button>
-            </div>
-            <div>
-                <Button
-                    secondary
-                    outline
-                >
-                    Hide Ads!
-                </Button>
-            </div>
-            <div>
-                <Button
-                    primary
-                    rounded
-                >
-                    Something!
-                </Button>
-            </div>
-        </div>
-    );
+	const items = [
+		{
+			id: 'qwe23r',
+			label: 'Can I use React on a project?',
+			content: 'You can use React on any project you want.',
+		},
+		{
+			id: '5sdj',
+			label: 'Can I use Javascript on a project?',
+			content: 'You can use Javascript on any project you want.',
+		},
+		{
+			id: 'grbn3',
+			label: 'Can I use CSS on a project?',
+			content: 'You can use CSS on any project you want.',
+		},
+	];
+
+	return <Accordion items={items} />;
 }
 
 export default App;
