0c1a1ade677d7df238698aec6aeebdf59b01665a
arthurphung1 <rthrphung73@gmail.com>
2023-06-08 23:45:33 -0400

    modal and table comps

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -3,23 +3,38 @@ import Modal from '../components/Modal';
 import Button from '../components/Button';
 
 function ModalPage() {
-	const [showModal, setShowModal] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
-	const handleClick = () => {
-		setShowModal(true);
-	};
+    const handleClick = () => {
+        setShowModal(true);
+    };
 
-	return (
-		<div>
-			<Button
-				onClick={handleClick}
-				primary
-			>
-				Open Modal
-			</Button>
-			{showModal && <Modal />}
-		</div>
-	);
+    const handleClose = () => {
+        setShowModal(false);
+    };
+
+    const actionBar = (
+        <div>
+            <Button onClick={handleClose} primary>
+                I Accept
+            </Button>
+        </div>
+    );
+
+    const modal = (
+        <Modal onClose={handleClose} actionBar={actionBar}>
+            <p>Here is an important agreement for you to accept</p>
+        </Modal>
+    );
+
+    return (
+        <div className='relative'>
+            <Button primary onClick={handleClick}>
+                Open Modal
+            </Button>
+            {showModal && modal}
+        </div>
+    );
 }
 
 export default ModalPage;
